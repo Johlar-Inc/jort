a5e8eaa7e1dac74cc79026d057e3f70a6860d1fd
Raul Smith <raul.n.smith@gmail.com>
2022-11-27 15:15:43 -0700

    Initial edits

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/owl.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container-fluid px-0">
+      <div className="row">
+        <div className="col">
+          <main className="main">
+            <Component {...pageProps} />
+          </main>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
